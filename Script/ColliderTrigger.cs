03199fe7837af8a21c9bb0042c43107e22dc55b5
Mr_Gamr211 <118116138+MrGamr211@users.noreply.github.com>
2025-08-10 13:45:37 +0300

    Update ColliderTrigger.cs
    Now the the button must me Collided all the time to be active.

--- Script/ColliderTrigger.cs
+++ Script/ColliderTrigger.cs
@@ -6,11 +6,16 @@ public class ColliderTrigger : MonoBehaviour
     public GameObject Door;
     void Start()
     {
-        
+
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        Destroy(Door);
+        Door.SetActive(false);
     }
+    public void OnTriggerExit(Collider other)
+    {
+        Door.SetActive(true);
+    }
+
 }
